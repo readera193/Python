08ea5d0aad9838fbbc2272b1df6a272d4c06c8c9
1106108136 <1106108136@nkust.edu.tw>
2019-10-08 22:10:10 +0800

    Revert "feat[Chat_room]: 離開聊天室/QUIT"
    This reverts commit 3616b738acaf478b1c371399304b6350f57d2e26.

--- Chat_room.py
+++ Chat_room.py
@@ -1,7 +1,9 @@
 import argparse, socket
 import threading
+import json
 BUFSIZE = 80
-IRC_PORT = 6667
+REGISTRAR_PORT = 648
+EOF = 'oo'
 SOCKS = {}
 
 class recvThread(threading.Thread):
@@ -21,9 +23,6 @@ class sendThread(threading.Thread):
         while True:
             msg = input()
             self.sock.sendall(msg.encode('UTF-8'))
-            args = msg.split()
-            if len(args)==1 and args[0].upper()=="/QUIT":
-                break
             
 class serverThread(threading.Thread):
     def __init__(self, sock):
@@ -31,27 +30,15 @@ class serverThread(threading.Thread):
         self.sock = sock
     def run(self):
         sock = self.sock
-        # TODO: 使用者非使用指令離開/意外斷線時，需移除SOCKS內容
         while True:
             data = sock.recv(BUFSIZE).decode('UTF-8')
             args = data.split()
             if data[0]=='/':
-                if len(args)==2 and args[0].upper()=="/USER":
-                    if sock not in SOCKS.keys():
-                        print("User [{}\t] joined".format(args[1]))
-                    else:
-                        print("User [{}\t] rename as [{}\t]".format(SOCKS[sock], args[1]))
+                if len(args)==2 and args[0].lower()=="/user":
                     SOCKS[sock] = args[1]
-                elif len(args)==1 and args[0].upper()=="/WHO":
-                    for name in SOCKS.values():
-                        msg = "User [{}\t] is online".format(name)
-                        sock.sendall(msg.encode('UTF-8'))
-                elif len(args)==1 and args[0].upper()=="/QUIT":
-                    name = SOCKS.pop(sock)
-                    print("User [{}\t] left".format(name))
-                    break
+                    print("User {} joined".format(args[1]))
                 else:
-                    sock.sendall(b'Incorrect command')
+                    sock.sendall(b'error')
             elif sock in SOCKS.keys():
                 print(SOCKS[sock], " : ", data)
                 for otherSocks in SOCKS.keys():
@@ -65,7 +52,6 @@ def server(host, port):
     listeningSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     listeningSock.bind((host, port))
     listeningSock.listen(1)
-    print("Listening at", listeningSock.getsockname())
     while True:
         sock, sockname = listeningSock.accept()
         thread = serverThread(sock)
@@ -78,11 +64,10 @@ def client(host, port):
     print("Connected to", sock.getpeername())
     
     recv = recvThread(sock)
-    recv.daemon = True
     recv.start()
     send = sendThread(sock)
     send.start()
-    
+    recv.join()
     send.join()
     sock.close()
     
@@ -92,8 +77,8 @@ if __name__ == '__main__':
     parser.add_argument('role', choices=choices, help='which role to play')
     parser.add_argument('host', help='interface the server listens at;'
                         ' host the client sends to')
-    parser.add_argument('-p', metavar='PORT', type=int, default=IRC_PORT,
-                        help='TCP port (default 6667)')
+    parser.add_argument('-p', metavar='PORT', type=int, default=8136,
+                        help='TCP port (default 8136)')
     args = parser.parse_args()
     function = choices[args.role]
     function(args.host, args.p)
