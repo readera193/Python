2a57eedec498bfaba09de81a7cd144c2ecea4e05
1106108136 <1106108136@nkust.edu.tw>
2019-10-13 14:30:22 +0800

    feat[Instant Chatting]: 新增結束通訊
    調整項目:
    1. BUFSIZE增加，80=>150
    2. REGISTRAT_PORT更改，648=>5060
    3. 新增常數CODING，儲存編碼方式，目前編碼為UTF-8
    4. 儲存servers時，去除'role'欄位，減少空間浪費
    5. 當新server註冊時不再顯示所有server，只顯示新註冊的server
    6. client端選擇server時，開始的index調整，0=>1
    7. **server與client的收發訊息皆改成一個thread+一個while**
    8. 當client端輸入"oo"結束通訊
    
    Closes #4

--- Instant Chatting.py
+++ Instant Chatting.py
@@ -1,19 +1,20 @@
 import argparse, socket
 import threading
 import json
-BUFSIZE = 80
-REGISTRAR_PORT = 648
+BUFSIZE = 150
+REGISTRAR_PORT = 5060
 EOF = 'oo'
+CODING = 'UTF-8'
 
 class threadRecv(threading.Thread):
-    def __init__(self, sock, peerName):
+    def __init__(self, sock, serverName):
         threading.Thread.__init__(self)
         self.sock = sock
-        self.peerName = peerName
+        self.serverName = serverName
     def run(self):
         while True:
-            data = self.sock.recv(BUFSIZE)
-            print(self.peerName, ": ", data.decode('UTF-8'))
+            data = self.sock.recv(BUFSIZE).decode(CODING)
+            print(self.serverName, ": ", data)
 
 class threadSend(threading.Thread):
     def __init__(self, sock):
@@ -22,31 +23,30 @@ class threadSend(threading.Thread):
     def run(self):
         while True:
             msg = input()
-            self.sock.sendall(msg.encode('UTF-8'))
+            self.sock.sendall(msg.encode(CODING))
 
 def registrar(host, *args):
     servers = [];
     registrarSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     registrarSock.bind((host, REGISTRAR_PORT))
     registrarSock.listen(1)
-    print('Waiting to accept a new register')
+    print("Listening at", registrarSock.getsockname())
     
     while True:
         sock, sockname = registrarSock.accept()
-        peerInfo = sock.recv(BUFSIZE).decode('UTF-8')
+        peerInfo = sock.recv(BUFSIZE).decode(CODING)
         peerInfo = json.loads(peerInfo)
         if peerInfo['role'] == 'server':
+            peerInfo.pop('role')
             servers.append(peerInfo)
-            print("\nOnline server update:")
-            print("name", "address", sep='\t')
-            for server in servers:
-                print(server['name'], tuple(server['address']), sep='\t')
+            print("\nNew server {} registers as {}".format(peerInfo['name'], 
+                  tuple(peerInfo['address'])))
         elif peerInfo['role'] == 'client':
-            sock.sendall(json.dumps(servers).encode('UTF-8'))
+            sock.sendall(json.dumps(servers).encode(CODING))
         sock.close()
 
 def server(host, port):
-    print('Please input your name: ', end='')
+    print("Please input your name: ", end='')
     name = input()
     
     listeningSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -57,22 +57,23 @@ def server(host, port):
     registerSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     registerSock.connect((host, REGISTRAR_PORT))
     registerInfo = {'role':'server','name':name, 'address':listeningSock.getsockname()}
-    registerInfo = json.dumps(registerInfo)
-    registerSock.sendall(registerInfo.encode('UTF-8'))
+    registerSock.sendall(json.dumps(registerInfo).encode(CODING))
     registerSock.close()
     
     sock, sockname = listeningSock.accept()
     print("We have accepted a connection from", sockname)
-    clientName = sock.recv(BUFSIZE).decode('UTF-8')
+    clientName = sock.recv(BUFSIZE).decode(CODING)
     
-    recv = threadRecv(sock, clientName)
-    recv.start()
     send = threadSend(sock)
+    send.daemon = True
     send.start()
     
-    recv.join()
-    send.join()
-    print("Your peer {} closes the connection.".format(sock.getpeername() ))
+    data = None
+    while data != EOF:
+        data = sock.recv(BUFSIZE).decode(CODING)
+        print(clientName, ": ", data)
+    
+    print("Your peer {} closes the connection.".format(sock.getpeername()))
     sock.close()
     listeningSock.close()
     
@@ -81,9 +82,9 @@ def client(host, *args):
     name = input()
     getServers = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     getServers.connect((host, REGISTRAR_PORT))
-    getServers.sendall(json.dumps({'role':'client'}).encode('UTF-8'))
-    servers = json.loads(getServers.recv(BUFSIZE).decode('UTF-8'))
-    index = 0
+    getServers.sendall(json.dumps({'role':'client'}).encode(CODING))
+    servers = json.loads(getServers.recv(BUFSIZE).decode(CODING))
+    index = 1
     print("\nOnline server:")
     print("num", "name", "address", sep='\t')
     for server in servers:
@@ -93,26 +94,29 @@ def client(host, *args):
     print("Input server number to connect: ", end='')
     while True:
         try:
-            select = input()
-            host, port = servers[int(select)]['address']
-            serverName = servers[int(select)]['name']
-            break
-        except:
+            select = int(input()) - 1
+            host, port = servers[select]['address']
+            serverName = servers[select]['name']
+        except (IndexError, ValueError):
             print("Wrong choice, please input currect number: ", end='')
             continue
+        else:
+            break
     
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((host, port))
     print("Connected to", sock.getpeername())
-    sock.sendall(name.encode('UTF-8'))
+    sock.sendall(name.encode(CODING))
     
     recv = threadRecv(sock, serverName)
+    recv.daemon = True
     recv.start()
-    send = threadSend(sock)
-    send.start()
     
-    recv.join()
-    send.join()
+    msg = None
+    while msg != EOF:
+        msg = input()
+        sock.sendall(msg.encode(CODING))
+    
     sock.close()
     
 if __name__ == '__main__':
