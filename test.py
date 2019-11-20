1f9c73446d96fb6d7f3a13c5eab8fa9ab8dc3012
1106108136 <1106108136@nkust.edu.tw>
2019-11-20 15:16:47 +0800

    feat: MergeSortWithPID
    1. MergeSort.py save as MergeSortWithPID.py
    
    issue #39

--- test.py
+++ test.py
@@ -1,5 +1,12 @@
+class Test():
+    PIDs = []
+    def __init__(self):
+        self.PIDs.append(8)
+
 def main():
-    print("test")
+    for i in range(2):
+        Test()
+    print(Test.PIDs)
 
 if __name__ == '__main__':
     main()
