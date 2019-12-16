bbb3268328c9ecb0c9d8f21ace122f12c6684846
readera193 <50070666+readera193@users.noreply.github.com>
2019-12-16 14:37:34 +0800

    feat: Q2 server端
    新增Q2 server端
    
    issue #43

--- 191216_Q2.py
+++ 191216_Q2.py
@@ -2,6 +2,8 @@ import argparse, socket
 
 MAX_BYTES = 65535
 
+ID = ['']
+
 def client(port, host):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     text = "1106108136"
@@ -11,9 +13,26 @@ def client(port, host):
     data, address = sock.recvfrom(MAX_BYTES)  # Danger! See Chapter 2
     text = data.decode()
     print('The server {} replied {!r}'.format(address, text))
+    
+def server(port, host):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind((host, port))
+    print('Listening at {}'.format(sock.getsockname()))
+    while True:
+        data, address = sock.recvfrom(MAX_BYTES)
+        student_id = data.decode()
+        if student_id in ID:
+            text = "{} has already logged in as the {}nd student.".format(student_id, ID.index(student_id))
+        else:
+            text = "Welcome, {}, you're the {}nd to login.".format(student_id, len(ID))
+            ID.append(student_id)
+            
+        print(text)
+        data = text.encode()
+        sock.sendto(data, address)
 
 if __name__ == '__main__':
-    choices = {'client': client}
+    choices = {'client': client, 'server': server}
     parser = argparse.ArgumentParser(description='Send and receive UDP locally')
     parser.add_argument('role', choices=choices, help='which role to play')
     parser.add_argument('host', help='interface the server listens at;'
