2591592a267ea2aa5285125d1f206f61f8988e09
readera193 <50070666+readera193@users.noreply.github.com>
2019-12-16 14:18:11 +0800

    feat: Q1 server端
    新增Q1 server端
    
    issue #43

--- 191216_Q1.py
+++ 191216_Q1.py
@@ -1,5 +1,13 @@
 import argparse, socket
 
+QUESTION = '''
+1. The server is listening at exam.ipv6.club.tw:8565 with UDP.
+2. If a client sends a string of student ID (actually, a bytes array,
+   such as b"1106108123") to the server, the server will reply a
+   message.
+3. If the student ID belongs to a student enrolled to this class,
+   the server will reply the name of the student.'''
+
 def client(host, port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((host, port))
@@ -7,9 +15,25 @@ def client(host, port):
     reply = sock.recv(500)
     print(reply.decode())
     sock.close()
+    
+def server(interface, port):
+    listeningSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    listeningSock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    listeningSock.bind((interface, port))
+    listeningSock.listen(1)
+    print('Listening at', listeningSock.getsockname())
+    
+    while True:
+        print('Waiting to accept a new connection')
+        sock, sockname = listeningSock.accept()
+        print('We have accepted a connection from', sockname)
+        print('  Socket name:', sock.getsockname())
+        print('  Socket peer:', sock.getpeername())
+        sock.sendall(QUESTION.encode())
+        sock.close()
 
 if __name__ == '__main__':
-    choices = {'client': client}
+    choices = {'client': client, 'server': server}
     parser = argparse.ArgumentParser(description='Send and receive over TCP')
     parser.add_argument('role', choices=choices, help='which role to play')
     parser.add_argument('host', help='interface the server listens at;'
