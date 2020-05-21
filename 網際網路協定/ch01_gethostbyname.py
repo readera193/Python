18dda47426ec30500ff25afbccd3d5a5da189ec7
readera193 <readera193@gmail.com>
2020-05-21 14:57:56 +0800

    refactor: 整理程式碼
    將網際網路協定相關程式碼整理進資料夾
    
    issue #51

---
+++ 網際網路協定/ch01_gethostbyname.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# Foundations of Python Network Programming, Third Edition
+# https://github.com/brandon-rhodes/fopnp/blob/m/py3/chapter01/getname.py
+
+import socket
+
+if __name__ == '__main__':
+    hostname = 'www.python.org'
+    addr = socket.gethostbyname(hostname)
+    print('The IP address of {} is {}'.format(hostname, addr))
+
